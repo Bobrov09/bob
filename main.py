c71a4ae1e4a82c71da87e8a1a7c7637f228f50e6
bobrov_di <bobrov_di@taximaxim.ru>
2025-08-02 15:04:37 +0300

    kaka

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fractions import Fraction
 
 try:
     class Calculator:
@@ -11,9 +12,6 @@ try:
             return a * b
 
 
-        def divide(self, a, b):
-            return a / b
-
 except ZeroDivisionError:
     print("на ноль делить нельзя")
 
@@ -26,11 +24,12 @@ class AdvancedCalculator(Calculator):
         import math
         return math.sqrt(a)
 
-
+    def divide(self, a, b):
+        return Fraction(a / b)
 
 calc = Calculator()
 print(calc.add(5, 3))
-print(calc.divide(10, 2))
 adv_calc = AdvancedCalculator()
+print(calc.divide(3, 4))
 print(adv_calc.power(2, 3))
 print(adv_calc.sqrt(16))
